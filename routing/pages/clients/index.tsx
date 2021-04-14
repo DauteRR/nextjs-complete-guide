1b31a23f2eee8ecb1736cf88a140f77b32f439b2
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-04-14 20:14:46 +0100

    [Section 3] Navigation through pages

--- routing/pages/clients/index.tsx
+++ routing/pages/clients/index.tsx
@@ -1,9 +1,31 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const ClientsPage: NextPage = () => {
+	const clients = [
+		{ id: 'peter', name: 'Peter Parker' },
+		{ id: 'max', name: 'Maximilian' },
+	];
+
 	return (
 		<div>
 			<h1>The Clients Page</h1>
+			<ul>
+				{clients.map(client => (
+					<li key={client.id}>
+						<Link
+							href={{
+								pathname: '/clients/[clientId]',
+								query: {
+									clientId: client.id,
+								},
+							}}
+						>
+							{client.name}
+						</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
--- routing/pages/index.tsx
+++ routing/pages/index.tsx
@@ -1,9 +1,18 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const HomePage: NextPage = () => {
 	return (
 		<div>
 			<h1>The Home Page</h1>
+			<ul>
+				<li>
+					<Link href="/portfolio">Portfolio</Link>
+				</li>
+				<li>
+					<Link href="/clients">Clients</Link>
+				</li>
+			</ul>
 		</div>
 	);
 };
