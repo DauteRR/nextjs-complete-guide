dd389b9880da849fc522c2fe3192ff0ca940922e
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-04-14 20:20:48 +0100

    [Section 3] Programmatic navigation and 404 page

---
+++ routing/pages/404.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+const NotFoundPage: NextPage = () => {
+	return (
+		<div>
+			<h1>404 Page not found!</h1>
+		</div>
+	);
+};
+
+export default NotFoundPage;
--- routing/pages/clients/[ clientId]/index.tsx
+++ routing/pages/clients/[ clientId]/index.tsx
@@ -1,9 +1,23 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/dist/client/router';
 
 const ClientProjectsPage: NextPage = () => {
+	const router = useRouter();
+
+	const loadProjectHandler = () => {
+		router.push({
+			pathname: '/clients/[clientId]/[clientProjectId]',
+			query: {
+				clientId: 'max',
+				clientProjectId: 'project-a',
+			},
+		});
+	};
+
 	return (
 		<div>
 			<h1>The Projects of a Given Client</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
 		</div>
 	);
 };
