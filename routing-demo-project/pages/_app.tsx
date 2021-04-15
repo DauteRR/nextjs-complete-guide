35018ce22745318dc05e25b0766a30d33ba9d5f7
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-04-15 21:20:14 +0100

    [Section 4] Common layout

--- routing-demo-project/pages/_app.tsx
+++ routing-demo-project/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Layout from '../components/layout/layout';
 
 const App = (props: AppProps) => {
 	const { Component, pageProps } = props;
 
-	return <Component {...pageProps} />;
+	return (
+		<Layout>
+			{' '}
+			<Component {...pageProps} />
+		</Layout>
+	);
 };
 
 export default App;
