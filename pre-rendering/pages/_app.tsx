cc2692d747160089b267721dc72effd12b9e8d74
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-04-20 19:12:02 +0100

    [Section 5] getStaticProps

--- pre-rendering/pages/_app.tsx
+++ pre-rendering/pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
 const App = (props: AppProps) => {
--- pre-rendering/pages/index.tsx
+++ pre-rendering/pages/index.tsx
@@ -1,15 +1,38 @@
+import { GetStaticProps } from 'next';
 import React from 'react';
+import fs from 'fs/promises';
+import path from 'path';
 
-export interface HomePageProps {}
+export interface Product {
+	id: string;
+	title: string;
+}
 
-export const HomePage: React.FC<HomePageProps> = () => {
+export interface HomePageProps {
+	products: Product[];
+}
+
+export const HomePage: React.FC<HomePageProps> = ({ products }) => {
 	return (
 		<ul>
-			<li>Product 1</li>
-			<li>Product 2</li>
-			<li>Product 3</li>
+			{products.map(product => (
+				<li key={product.id}>{product.title}</li>
+			))}
 		</ul>
 	);
+};
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async context => {
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+	const jsonData = await fs.readFile(filePath);
+	const data: { products: Product[] } = JSON.parse(jsonData.toString());
+
+	return {
+		props: {
+			products: data.products,
+		},
+	};
 };
 
 export default HomePage;
