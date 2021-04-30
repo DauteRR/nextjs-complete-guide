9f486e24b77191817dc425a2661e3ec102468e67
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-04-30 20:31:57 +0100

    [Section 11] PostDetails SSG

--- blog/pages/posts/[slug].tsx
+++ blog/pages/posts/[slug].tsx
@@ -1,7 +1,13 @@
-import { NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import React from 'react';
 import PostContent from '../../components/posts/post-detail/post-content';
 import { Post } from '../../types';
+import { getPostData, getPostsFiles } from '../../utils/posts';
+
+export interface PostPageParsedUrlQuery {
+	[key: string]: string | string[];
+	slug: string;
+}
 
 export interface PostPageProps {
 	post: Post;
@@ -9,6 +15,40 @@ export interface PostPageProps {
 
 const PostPage: NextPage<PostPageProps> = ({ post }) => {
 	return <PostContent {...post} />;
+};
+
+export const getStaticProps: GetStaticProps<PostPageProps, PostPageParsedUrlQuery> = async ({
+	params,
+}) => {
+	const { slug } = params;
+
+	const post = getPostData(slug);
+
+	if (post === undefined) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: {
+			post,
+		},
+		revalidate: 600,
+	};
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const slugs = getPostsFiles().map(fileName => fileName.replace(/\.md/, ''));
+
+	return {
+		fallback: false,
+		paths: slugs.map(slug => ({
+			params: {
+				slug,
+			},
+		})),
+	};
 };
 
 export default PostPage;
--- blog/utils/posts.ts
+++ blog/utils/posts.ts
@@ -5,27 +5,34 @@ import { Post } from '../types';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-function getPostData(fileName: string): Post {
-	const filePath = path.join(postsDirectory, fileName);
-	const fileContent = fs.readFileSync(filePath, 'utf-8');
-	const { data, content } = matter(fileContent);
+export function getPostData(postId: string): Post | undefined {
+	const postSlug = postId.replace(/\.md/, '');
+	const filePath = path.join(postsDirectory, `${postSlug}.md`);
 
-	const slug = fileName.replace(/\.md/, '');
+	try {
+		const fileContent = fs.readFileSync(filePath, 'utf-8');
+		const { data, content } = matter(fileContent);
 
-	return {
-		content,
-		date: data.date ?? '',
-		description: data.description ?? '',
-		image: data.image ?? '',
-		isFeatured: data.isFeatured ?? false,
-		slug,
-		title: data.title ?? '',
-	};
+		return {
+			content,
+			date: data.date ?? '',
+			description: data.description ?? '',
+			image: data.image ?? '',
+			isFeatured: data.isFeatured ?? false,
+			slug: postSlug,
+			title: data.title ?? '',
+		};
+	} catch (err) {
+		return undefined;
+	}
+}
+
+export function getPostsFiles(): string[] {
+	return fs.readdirSync(postsDirectory);
 }
 
 export function getAllPosts(): Post[] {
-	const postFiles = fs.readdirSync(postsDirectory);
-	const posts = postFiles.map(getPostData);
+	const posts = getPostsFiles().map(getPostData);
 
 	return posts.sort((postA, postB) => (postA.date > postB.date ? -1 : 1));
 }
